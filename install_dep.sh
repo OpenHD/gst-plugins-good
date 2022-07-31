5b56ea10a536d012f4d670573e4575966102540c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-31 15:29:12 +0200

    add backports

--- install_dep.sh
+++ install_dep.sh
@@ -3,7 +3,7 @@
 # Install all the dependencies needed to build QOpenHD from source.
 
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good \
-    ruby ruby-dev rubygems
+    ruby ruby-dev/buster-backports rubygems/buster-backports
 apt -y install git meson fmt
 
 gem install fpm
