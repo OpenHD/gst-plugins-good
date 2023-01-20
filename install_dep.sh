9857c9f9d32fa0988858ceb3f578b29381fc982f
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-20 02:00:40 +0100

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -3,6 +3,6 @@
 # Install all the dependencies needed to build QOpenHD from source.
 
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
-apt -y install git meson fmt
+apt -y install git meson
 
 gem install fpm
